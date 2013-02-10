2a3733cb2f7c60bac325b5958c5e58662932b362
Giordano Battilana <giordano.battilana@novadart.com>
2013-02-10 19:00:28 +0100

    default style for loader button

--- src/main/java/com/novadart/gwtshared/client/LoaderButton.java
+++ src/main/java/com/novadart/gwtshared/client/LoaderButton.java
@@ -1,8 +1,11 @@
 package com.novadart.gwtshared.client;
 
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
@@ -12,22 +15,40 @@ import com.google.gwt.user.client.ui.SimplePanel;
 import com.novadart.gwtshared.client.resources.ImageResources;
 
 public class LoaderButton extends Composite implements HasClickHandlers, HasText {
+	
+	public interface Style extends CssResource {
+		String button();
+		String loader();
+	}
+	
+	public interface DefaultBundle extends ClientBundle {
+		@Source("LoaderButton.css")
+		Style defaultStyle();
+	}
+	
+	private static DefaultBundle defaultBundle = null;
+	private static DefaultBundle defaultBundle() {
+		if(defaultBundle == null){
+			defaultBundle = (DefaultBundle)GWT.create(DefaultBundle.class);
+		}
+		return defaultBundle;
+	}
 
 	private final SimplePanel wrapper = new SimplePanel();
 	private final Button button = new Button();
 	private final Image loader = new Image();
 	
 	public LoaderButton() {
-		this(ImageResources.INSTANCE.loader());
+		this(ImageResources.INSTANCE.loader(), defaultBundle().defaultStyle());
 	}
 	
-	public LoaderButton(ImageResource loaderImage) {
+	public LoaderButton(ImageResource loaderImage, Style style) {
 		loader.setUrl(loaderImage.getSafeUri());
 		initWidget(wrapper);
 		wrapper.setWidget(button);
 		setStyleName("LoaderButton");
-		button.setStyleName(getStylePrimaryName()+"-button");
-		loader.setStyleName(getStylePrimaryName()+"-loader");
+		button.setStyleName(style.button());
+		loader.setStyleName(style.loader());
 	}
 	
 	public void showLoader(boolean value){
